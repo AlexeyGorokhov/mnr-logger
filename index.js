c5248861132bf78f5cb3a7471005c385a13e74a5
Alexey Gorokhov <alexey.gorokhov@mail.ru>
2018-09-03 15:35:32 +0300

    Start development

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
 'use strict';
+
+const winston = require('winston');
+
+let logger;
+
+module.exports = { error, warn, info, debug };
+
+function error (message, stack) {
+  log('error', message, stack);
+}
+
+function warn (message, meta) {
+  log('warn', message, meta);
+}
+
+function info (message, meta) {
+  log('info', message, meta);
+}
+
+function debug (message, meta) {
+  log('debug', message, meta);
+}
+
+function log (level, message, meta) {
+  if (!logger) logger = createLogger();
+  logger.log(level, message, meta);
+}
+
+function createLogger () {
+  let level;
+
+  if (process.env.NODE_ENV === 'production') {
+    level = 'info';
+  } else {
+    level = 'debug';
+  }
+
+  logger = winston.createLogger({
+    transports: [
+      new winston.transports.Console({
+        level: level,
+        stderrLevels: ['error', 'warn'],
+        timestamp () { return (new Date()).toISOString(); }
+      })
+    ]
+  });
+
+  return logger;
+}
